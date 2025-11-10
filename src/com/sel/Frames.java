2290cc3ccec036071182fc780d5b8f188118d119
Arthi1018 <101647329+Arthi1018@users.noreply.github.com>
2025-11-10 10:02:55 +0530

    Update Frames.java

--- src/com/sel/Frames.java
+++ src/com/sel/Frames.java
@@ -12,38 +12,6 @@ public class Frames {
 	
 public static void main(String[] args) throws IOException, InterruptedException, AWTException {
 		
-		//Object creation- Browser Launch
-		WebDriver driver=new ChromeDriver(); //upcasting
-		
-		//Pass the URL
-		driver.get("https://leafground.com/frame.xhtml;jsessionid=node05z4mvbp3y1j4tde0eocr7o5q7686971.node0");
-		
-		//Maximize the window
-		driver.manage().window().maximize();
-		
-		WebElement single = driver.findElement(By.xpath("//*[@id=\"j_idt88\"]/div/div[1]/div[1]/iframe"));
-		
-		driver.switchTo().frame(single);
-		
-		driver.findElement(By.id("Click")).click();
-		
-		driver.switchTo().defaultContent();
-		
-		//MULTIPLE FRAME
-		
-		WebElement outer = driver.findElement(By.xpath("//iframe[@src='page.xhtml']"));
-		
-		driver.switchTo().frame(outer);
-		
-		driver.switchTo().frame("frame2");
-		
-		driver.findElement(By.id("Click")).click();
-		
-	//	driver.switchTo().parentFrame();
-		
-		driver.switchTo().defaultContent();
-		
-		driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[1]/ul/li[1]/input")).sendKeys("Arthi");
 		
 		
 }
